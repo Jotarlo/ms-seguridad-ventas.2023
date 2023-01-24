9386b4665568c6973189dbbe0f19b624e9667a16
jotarlo <Jeferson Arango Lpez>
2023-01-23 22:20:56 -0500

    Creación de datasource con mongodb y cambio de puerto en el servidor por el 3001

---
+++ src/datasources/mongodb.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongodb',
+  connector: 'mongodb',
+  url: 'mongodb+srv://user_mongo_ventas:Admin2023@clustermongo.42alvam.mongodb.net/seguridad_ventas?retryWrites=true&w=majority',
+  host: 'localhost',
+  port: 27017,
+  user: '',
+  password: '',
+  database: 'seguridad_ventas',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongodbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mongodb';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongodb', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
